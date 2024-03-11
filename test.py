8e1bb8c3478d9a0ffb014fba3f98f07b8820d1f6
camarota <joao.camarota@discente.ufg.br>
2024-03-11 19:27:26 -0300

    mudando pastas

--- test.py
+++
@@ -1,9 +0,0 @@
-
-import string
-letters_guessed = "abc"
-available = ""
-for letter in string.ascii_lowercase:
-    if letter not in letters_guessed:
-        available += letter
-
-print(available)
